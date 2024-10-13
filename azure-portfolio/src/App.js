90e2ab791a3ba3db688ab5718922bb1fd2bce795
AzureAsteroid2 <77765010+AzureAsteroid2@users.noreply.github.com>
2024-10-13 09:19:41 -0600

    simple HomePage view. Fixed imports

--- azure-portfolio/src/App.js
+++ azure-portfolio/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
+import HomePage from './views/HomePage/HomePage';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <div className="tab-content col-md-10  col-10 d-flex align-items-center justify-content-center">
-      {/* All pages should be declared here */}
+
       <Routes>
-        <Route></Route>
+        <Route path="/"/>
+        <Route index element={<HomePage/>} />
+        <Route path="*" element={<HomePage />} /> {/* Default if url doesn't exist.*/}
       </Routes>
       </div>
     </div>
---
+++ azure-portfolio/src/views/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Row from 'react-bootstrap/Row'
+import Col from "react-bootstrap/Col";
+const HomePage = () =>{
+    return(
+        <div>
+        <main>
+    <Row>
+        <Col>Test</Col>
+    </Row>
+    </main>
+    </div>
+    );
+};
+export default HomePage;
